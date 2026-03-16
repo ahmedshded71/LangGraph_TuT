1ba55c090dff002f1778e4e966a4d34d463f166c
ahmed_saad <ahmedshded71@gmail.com>
2026-03-16 23:18:52 +0200

    ChatBootAgent end TUT

--- LLMEngein/LLMProvidor/OllamaProvidor.py
+++ LLMEngein/LLMProvidor/OllamaProvidor.py
@@ -6,10 +6,16 @@ from ..LLMInterface import LLMInterface
 class OllamaProvidor(LLMInterface):
     def __init__(self):
         self.llm = ChatOllama(
-            model=os.getenv("OLLAMA_MODEL_NAME"),
-            temperature=0,
-            api_key=os.getenv("OLLAMA_API_KEY")
-        )
+                    model=os.getenv("OLLAMA_MODEL_NAME"),
+                    temperature=0,
+                    base_url=os.getenv("OLLAMA_API_KEY"), # هنا نضع رابط ngrok
+                    client_kwargs={
+                        "headers": {
+                            "ngrok-skip-browser-warning": "true",
+                            "Content-Type": "application/json"
+                        }
+                    }
+                )
 
     def GetLLM(self):
         return self.llm
